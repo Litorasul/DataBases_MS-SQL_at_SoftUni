60950f1caa535c5b3678040ad016f89f49affd23
Nikolay Kelov <n_kelov@yahoo.com>
2019-09-30 16:54:53 +0200

    Problem 16 - 18

--- 03.Built-in_functions/03.Built-in_Solutions.sql
+++ 03.Built-in_functions/03.Built-in_Solutions.sql
@@ -128,3 +128,38 @@ SELECT
 	RIGHT(Email, LEN(Email) - CHARINDEX('@', email)) AS [Email Provider]
 FROM Users
 ORDER BY [Email Provider], Username
+
+--Problem 16--
+SELECT 
+	Username,
+	IpAddress
+FROM Users
+WHERE IpAddress LIKE '___.1_%._%.___'
+ORDER BY Username
+
+--Problem 17--
+SELECT
+	[Name] AS [Game],
+	CASE
+		WHEN DATEPART(HOUR, [Start]) BETWEEN 0 AND 11 THEN 'Morning'
+		WHEN DATEPART(HOUR, [Start]) BETWEEN 12 AND 17 THEN 'Afternoon'
+		WHEN DATEPART(HOUR, [Start]) BETWEEN 18 AND 23 THEN 'Evening'
+	END AS [Part of the Day],
+	CASE
+		WHEN Duration <= 3 THEN 'Extra Short'
+		WHEN Duration BETWEEN 4 AND 6 THEN 'Short'
+		WHEN Duration > 6 THEN 'Long'
+		WHEN Duration IS NULL THEN 'Extra Long'
+	END AS [Duration]
+FROM Games
+ORDER BY Game, Duration
+
+--Problem 18--
+USE Orders
+
+SELECT 
+	ProductName,
+	OrderDate,
+	DATEADD(DAY, 3, OrderDate) AS [Pay Due],
+	DATEADD(MONTH, 1, OrderDate) AS [Deliver Due]
+FROM Orders
