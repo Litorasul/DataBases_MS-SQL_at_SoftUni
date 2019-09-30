73292f194a5ac003f8f58405739359c172b87bb5
Nikolay Kelov <n_kelov@yahoo.com>
2019-09-30 15:34:35 +0200

    Problem 10 - 15

--- 03.Built-in_functions/03.Built-in_Solutions.sql
+++ 03.Built-in_functions/03.Built-in_Solutions.sql
@@ -79,4 +79,52 @@ FROM Employees
 WHERE Salary BETWEEN 10000 AND 50000
 ORDER BY Salary DESC
 
+--Problem 11--
+SELECT
+	*
+FROM (SELECT
+	EmployeeID,
+	FirstName,
+	LastName,
+	Salary,
+DENSE_RANK() OVER(PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
+FROM Employees
+WHERE Salary BETWEEN 10000 AND 50000) AS Temp
+WHERE Temp.[Rank] = 2
+ORDER BY Salary DESC
 
+--Problem 12--
+USE [Geography]
+
+SELECT
+	CountryName,
+	IsoCode
+FROM Countries
+WHERE CountryName LIKE '%A%A%A%'
+ORDER BY IsoCode
+
+--Problem 13--
+SELECT
+	p.PeakName,
+	r.RiverName,
+	LOWER(CONCAT(LEFT(p.PeakName, LEN(p.PeakName) - 1), r.RiverName)) AS Mix
+FROM Peaks AS p, Rivers AS r
+WHERE RIGHT(p.PeakName, 1) = LEFT(r.RiverName, 1)
+ORDER BY Mix
+
+--Problem 14--
+USE Diablo
+
+SELECT TOP 50
+	[Name],
+	FORMAT(Start, 'yyyy-MM-dd') AS [Start]
+FROM Games
+WHERE DATEPART(YEAR, Start) BETWEEN '2011' AND '2012'
+ORDER BY [Start]
+
+--Problem 15--
+SELECT
+	Username,
+	RIGHT(Email, LEN(Email) - CHARINDEX('@', email)) AS [Email Provider]
+FROM Users
+ORDER BY [Email Provider], Username
