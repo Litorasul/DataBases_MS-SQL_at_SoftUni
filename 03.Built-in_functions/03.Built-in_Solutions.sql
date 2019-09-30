33d9a534c744e2cc07cda0d95c2c109e70ce06d1
Nikolay Kelov <n_kelov@yahoo.com>
2019-09-30 14:47:03 +0200

    Problem 05 - 10

--- 03.Built-in_functions/03.Built-in_Solutions.sql
+++ 03.Built-in_functions/03.Built-in_Solutions.sql
@@ -34,3 +34,49 @@ SELECT
 FROM Towns
 WHERE LEN([Name]) BETWEEN 5 AND 6
 ORDER BY [Name]
+
+--Problem 06--
+SELECT
+	*
+FROM Towns
+WHERE [Name] LIKE '[MKBE]%'
+ORDER BY [Name]
+
+--Problem 07--
+SELECT
+	*
+FROM Towns
+WHERE [Name] NOT LIKE '[RBD]%'
+ORDER BY [Name]
+
+--Problem 08--
+CREATE VIEW V_EmployeesHiredAfter2000 AS
+SELECT 
+	FirstName,
+	LastName
+FROM Employees
+WHERE DATEPART(YEAR,HireDate) > 2000
+
+SELECT 
+	*
+FROM V_EmployeesHiredAfter2000
+
+--Problem 09--
+SELECT 
+	FirstName,
+	LastName
+FROM Employees
+WHERE LEN(LastName) = 5
+
+--Problem 10--
+SELECT
+	EmployeeID,
+	FirstName,
+	LastName,
+	Salary,
+DENSE_RANK() OVER(PARTITION BY Salary ORDER BY EmployeeID) AS [Rank]
+FROM Employees
+WHERE Salary BETWEEN 10000 AND 50000
+ORDER BY Salary DESC
+
+
