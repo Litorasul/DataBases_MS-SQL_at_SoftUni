4bf28cce0ad7136388930d301bac36e7d37c0cfa
Nikolay Kelov <n_kelov@yahoo.com>
2019-10-01 13:02:12 +0200

    Problem 10 - 14

--- 04.Data_Aggregation/04.DataAggregation_Solutions.sql
+++ 04.Data_Aggregation/04.DataAggregation_Solutions.sql
@@ -88,3 +88,46 @@ SELECT
 FROM WizzardDeposits
 WHERE DepositGroup = 'Troll Chest'
 GROUP BY LEFT(FirstName, 1)
+
+--Problem 11--
+SELECT 
+	DepositGroup,
+	IsDepositExpired,
+	AVG(DepositInterest) AS [AverageInterest]
+FROM WizzardDeposits
+WHERE DATEPART(YEAR, DepositStartDate) > 1984 
+GROUP BY DepositGroup, IsDepositExpired
+ORDER BY DepositGroup DESC, IsDepositExpired
+
+--Problem 12--
+SELECT
+ SUM([Difference]) AS [SumDifference]
+ FROM(SELECT 
+	FirstName AS [Host Wizard],
+	DepositAmount AS [Host Wizard Deposit],
+	LEAD(FirstName) OVER (ORDER BY Id) AS [Guest Wizard],
+	LEAD(DepositAmount) OVER (ORDER BY Id) AS [Guest Wizard Deposit],
+	DepositAmount - LEAD(DepositAmount) OVER (ORDER BY Id) AS [Difference]
+FROM WizzardDeposits) AS [DiffTable]
+
+--Problem 13--
+USE SoftUni
+
+SELECT
+	DepartmentID,
+	SUM(Salary) AS [TotalSalary]
+FROM Employees
+GROUP BY DepartmentID
+ORDER BY DepartmentID
+
+--Problem 14--
+SELECT
+	DepartmentID,
+	MIN(Salary) AS [MinimumSalary]
+FROM Employees
+WHERE DATEPART(YEAR, HireDate) > 1999
+AND DepartmentID = 2 
+OR DepartmentID = 5
+OR DepartmentID = 7
+GROUP BY DepartmentID
+
