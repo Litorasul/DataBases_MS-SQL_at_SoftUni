3a0deadabde3438167ba5b4c50e979fad9e7f944
Nikolay Kelov <n_kelov@yahoo.com>
2019-10-06 10:50:33 +0200

    Problem 01

---
+++ 05.Table_Relations/Problem01.sql
@@ -0,0 +1,26 @@
+CREATE TABLE Persons(
+Id INT NOT NULL IDENTITY(1, 1) PRIMARY KEY, 
+FirstName NVARCHAR(200) NOT NULL,
+Salary DECIMAL(18, 2) NOT NULL,
+PassportID INT NOT NULL
+)
+
+CREATE TABLE Passports(
+PassportID INT NOT NULL IDENTITY(101, 1) PRIMARY KEY, 
+PassportNumber NVARCHAR(200) NOT NULL
+)
+
+
+ALTER TABLE Persons
+ADD CONSTRAINT FK_PassportID
+FOREIGN KEY (PassportID) REFERENCES Passports(PassportID)
+
+INSERT INTO Passports(PassportNumber) VALUES
+('N34FG21B'),
+('K65LO4R7'),
+('ZE657QP2')
+
+INSERT INTO Persons(FirstName, Salary, PassportID) VALUES
+('Roberto', 43300.00, 102),
+('Tom', 56100.00, 103),
+('Yana', 60200.00, 101)
