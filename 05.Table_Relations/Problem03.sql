5bc5f4a7edb9672c6d5db40364e9566ab9ebac58
Nikolay Kelov <n_kelov@yahoo.com>
2019-10-06 11:33:16 +0200

    Problem 03

---
+++ 05.Table_Relations/Problem03.sql
@@ -0,0 +1,43 @@
+CREATE TABLE Students(
+StudentID INT NOT NULL IDENTITY(1, 1) PRIMARY KEY,
+[Name] NVARCHAR(50) NOT NULL
+)
+
+CREATE TABLE Exams(
+ExamID INT NOT NULL IDENTITY(101, 1) PRIMARY KEY,
+[Name] NVARCHAR(50)
+)
+
+CREATE TABLE StudentsExams(
+StudentID INT NOT NULL,
+ExamID INT NOT NULL,
+CONSTRAINT PK_StudentsExams PRIMARY KEY (StudentID, ExamID)
+)
+
+
+ALTER TABLE StudentsExams
+ADD CONSTRAINT FK_StudentID
+FOREIGN KEY (StudentID) REFERENCES Students(StudentID)
+
+
+ALTER TABLE StudentsExams
+ADD CONSTRAINT FK_ExamID
+FOREIGN KEY (ExamID) REFERENCES Exams(ExamID)
+
+INSERT INTO Students([Name]) VALUES
+('Mila'),
+('Toni'),
+('Ron')
+
+INSERT INTO Exams([Name]) VALUES
+('SpringMVC'),
+('Neo4j'),
+('Oracle 11g')
+
+INSERT INTO StudentsExams(StudentID, ExamID) VALUES
+(1, 101),
+(1, 102),
+(2, 101),
+(3, 103),
+(2, 102),
+(2, 103)
