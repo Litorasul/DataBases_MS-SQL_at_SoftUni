a9da7ea572210763a72c8de620537b0aec4f8edf
Nikolay Kelov <n_kelov@yahoo.com>
2019-10-08 11:42:56 +0200

    Problem 03

---
+++ 06.SubqueriesAndJoins/Problem03.sql
@@ -0,0 +1,9 @@
+SELECT
+	E.EmployeeID,
+	E.FirstName,
+	E.LastName,
+	D.[Name] AS [DepartmentName]
+FROM Employees AS E
+	JOIN Departments AS D ON E.DepartmentID = D.DepartmentID
+WHERE D.[Name] = 'Sales'
+ORDER BY E.EmployeeID
